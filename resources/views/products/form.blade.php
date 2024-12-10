bea8deec363fc2e01122e8493ed0d44efe500055
mika-kadrie <mika.kadrie@outlook.com>
2024-12-10 15:20:03 +0100

    Update form.blade.php

--- resources/views/products/form.blade.php
+++ resources/views/products/form.blade.php
@@ -1,19 +1,22 @@
-<x-template title="Form">
-    Ini Form
+<x-template title="Add new product">
+    <div class="container py-3">
+        <h1>{{ $title }}</h1>
+        <form class="was-validated" method="post" action="{{ route('products.store') }}">
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" name="name" id="name">
+              </div>
+              <div class="mb-3">
+                <label for="description" class="form-label">Description</label>
+                <textarea class="form-control" name="description" id="description"></textarea>
+              </div>
+              <div class="mb-3">
+                <label for="price" class="form-label">Price</label>
+                <input type="number" class="form-control" name="price" id="price">
+              </div>
+              <div class="mb-3">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+        </form>
+    </div>
 </x-template>
-<form action="{{ route('products.store') }}" method="POST">
-    @csrf
-    <div class="mb-3">
-        <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" required>
-    </div>
-    <div class="mb-3">
-        <label for="description" class="form-label">Description</label>
-        <textarea class="form-control" id="description" name="description" rows="3" required></textarea>
-    </div>
-    <div class="mb-3">
-        <label for="price" class="form-label">Price</label>
-        <input type="number" class="form-control" id="price" name="price" required>
-    </div>
-    <button type="submit" class="btn btn-success">Submit</button>
-</form>
